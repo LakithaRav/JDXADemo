040b3b2196e5a0c5ce6e0a7f06de7dfa002a54a4
Lakitha Sam <lakitha@fidenz.com>
2015-11-07 11:18:19 +0530

    JDXASetup Class comments added

--- app/src/main/java/demo/olc/com/jdxademo/JDXASetup.java
+++ app/src/main/java/demo/olc/com/jdxademo/JDXASetup.java
@@ -5,13 +5,17 @@ import android.content.ContextWrapper;
 import com.softwaretree.jdxandroid.BaseAppSpecificJDXSetup;
 import com.softwaretree.jdxandroid.JDXSetup;
 
+/**
+ * This class is used to setup JDXA ORM with an object relational mapping specification
+ * and to initialize the database per the mapping specification.
+ */
 public class JDXASetup extends BaseAppSpecificJDXSetup {
 
+    // Resource id for the object relational mapping (.jdx) file
     private static int ormId = R.raw.jdxspec;
 
     public static void initialize() {
         setORMFileResourceId(ormId);
-//        setJdxForAndroidLicenseKey(ormId, "aPE01.0cCJDXEdU6E6885Fu5i7d5uTdzevRloR9Tu7znRskRgNJDXi7LY122");
         setJdxForAndroidLicenseKey(ormId, "VPP01.0cCdNpNaU0ulc85uzulszsupzjdxRdNpuJDXi7LYI3ObBZ5nGI7438");
         setForceCreateSchema(ormId, false);
         //un-comment thie below line to enable debug mode, mode vary from 1 - 5
